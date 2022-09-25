fe50cfab04e3583951ff86bb45018efa1d100402
Lex <eklypssdev@gmail.com>
2022-09-26 01:35:24 +0300

    Add project files.

---
+++ archivetool/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+
+namespace archivetool
+{
+    internal static class Tool
+    {
+        private static string? _downloadPath;
+        private static string? _exePath;
+        private const string DOWNLOADER_EXE_URL = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe";
+        private const string EXE_NAME = "yt-dlp.exe";
+
+        private static async Task Main(string[] args)
+        {
+            _exePath = Path.Combine(Directory.GetCurrentDirectory(), "yt-dlp.exe");
+            _downloadPath = Path.Combine(Directory.GetCurrentDirectory(), "downloads");
+            Cleanup();
+            if (!File.Exists(_exePath))
+            {
+                Console.WriteLine("yt-dlp not found, downloading..");
+                using (var http = new HttpClient())
+                {
+                    var bytes = await http.GetByteArrayAsync(DOWNLOADER_EXE_URL);
+                    await File.WriteAllBytesAsync(_exePath, bytes);
+                }
+                Console.WriteLine("yt-dlp downloaded successfully.");
+            }
+            else
+            {
+                Console.WriteLine("yt-dlp found, checking for updates.");
+                Process.Start(new ProcessStartInfo(EXE_NAME, "-U") { UseShellExecute = true });
+            }
+            if (!Directory.Exists(_downloadPath))
+            {
+                Directory.CreateDirectory(_downloadPath);
+                Console.WriteLine("Download folder created.");
+            }
+            Console.WriteLine("Input an URL to archive.");
+            CheckInput();
+            Console.ReadKey();
+        }
+
+        private static void Cleanup()
+        {
+            var files = Directory.GetFiles(_downloadPath);
+            if (files.Length == 0) return;
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+            Console.WriteLine($"{files.Length} media files deleted.");
+        }
+
+        private static void CheckInput()
+        {
+            var url = Console.ReadLine();
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                Console.WriteLine("Invalid URL.");
+                CheckInput();
+                return;
+            }
+            Console.WriteLine("URL is valid. Starting.");
+            StartDownload(url);
+        }
+
+        private static void Archive(string name)
+        {
+            ZipFile.CreateFromDirectory(_downloadPath, Path.Combine(Directory.GetCurrentDirectory(), $"{name.Split('/').Last()} {DateTime.Now.ToString("dMyyyy")}.zip"));
+            Console.WriteLine("Archive created.");
+            Cleanup();
+            Console.WriteLine("Input an URL to archive.");
+            CheckInput();
+        }
+
+        private static async void StartDownload(string url)
+        {
+            var process = Process.Start(new ProcessStartInfo(EXE_NAME, $"{url} -P downloads") { UseShellExecute = true });
+            // Not even needed?
+            if (process?.Responding == true)
+            {
+                await process?.WaitForExitAsync();
+            }
+            else
+            {
+                Console.WriteLine("Error, process is not responding.");
+                return;
+            }
+            var files = Directory.GetFiles(_downloadPath);
+            if (files.Length == 0)
+            {
+                Console.WriteLine("No files downloaded.");
+                return;
+            }
+            Console.WriteLine($"Download completed, {files.Length} file(s) downloaded. Archiving.");
+            Archive(url);
+        }
+    }
+}
